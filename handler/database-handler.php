e25e3922a094c214179cf2e809a6898bb87a3efc
jag8932 <jacob.a.goodwillie@gmail.com>
2023-02-09 16:20:07 -0500

    heroku fixes

--- handler/database-handler.php
+++ handler/database-handler.php
@@ -1,6 +1,7 @@
 <?php 
 
 // Heroku ClearDB connection
+/*
 $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 $dbServername = $url["host"];
 $dbUsername = $url["user"];
@@ -9,3 +10,14 @@ $dbName = substr($cleardb_url["path"], 1);
 
 //connection 
 $conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
+*/
+
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
