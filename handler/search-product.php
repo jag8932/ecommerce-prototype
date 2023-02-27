1ee57e3f799c87a003bca355a62b57d3eb42ac25
jag8932 <jacob.a.goodwillie@gmail.com>
2023-02-26 21:02:28 -0500

    test

--- handler/search-product.php
+++ handler/search-product.php
@@ -4,9 +4,10 @@ include_once './database-handler.php';
 if (isset($_POST["submit"])) {
     $searchInput = $_POST["input"];
     $test = searchFor($conn, $searchInput);
-    
+
     if (searchFor($conn, $searchInput)) {
-        header("Location: ../index.php?item=$test");
+        $testprod = $test["prod_name"];
+        header("Location: ../index.php?item=$testprod");
         exit();
     } else {
         echo '<p class="error">No items found</p>';
