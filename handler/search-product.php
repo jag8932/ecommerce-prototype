bc21ccede54de9c6d07fcf78ef1d5f06c0a02a92
jag8932 <jacob.a.goodwillie@gmail.com>
2023-02-26 20:55:22 -0500

    final fixes

--- handler/search-product.php
+++ handler/search-product.php
@@ -14,7 +14,7 @@ if (isset($_POST["submit"])) {
 }
 
 function searchFor($conn, $input) {
-    $sql = "SELECT * FROM products WHERE prod_name = '$input'";
+    $sql = "SELECT * FROM products WHERE prod_name = ?;";
     $stmt = mysqli_stmt_init($conn);
 
 if (!mysqli_stmt_prepare($stmt, $sql)) {
