a5ee380faf8a10e72eaa811933675a89ac88dc8f
jag8932 <jacob.a.goodwillie@gmail.com>
2023-03-07 01:11:31 -0500

    api tests

--- index.php
+++ index.php
@@ -2,12 +2,19 @@
 <?php 
 include './header.php';
 
-if (isset($_GET[""])) {
-    
+if (isset($_GET["search"])) {
+    $test = $_GET["search"];
+    echo $test;
 }
 ?>
 
 <h1>To Come Soon!</h1>
+<template class="product-template">
+    <h2 class="product-name"><h2>
+    <img src="" class="product-image" width="100px"/>
+    <h3 class="product-tags"></h3>
+    <p class="product-description"></p>
+</template>
 <?php 
 // Will act as the main search page for products like how Amazon's home page looks. 
 include './footer.php';
