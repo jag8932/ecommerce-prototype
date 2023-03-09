9920954ec050550923e7160376b534fa3d88ab10
jag8932 <jacob.a.goodwillie@gmail.com>
2023-03-08 22:52:05 -0500

    test

--- index.php
+++ index.php
@@ -31,16 +31,10 @@ if (isset($_GET["search"])) {
     echo "</div>";
 
 }
-
 ?>
 
-<template class="product-template">
-    <h2 class="product-name"><h2>
-    <img src="" class="product-image" width="100px"/>
-    <h3 class="product-tags"></h3>
-    <p class="product-description"></p>
-</template>
-<?php 
-// Will act as the main search page for products like how Amazon's home page looks. 
+<h1>Featured</h1>
+
+<?php  
 include './footer.php';
 ?>
